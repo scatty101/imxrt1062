051931dd2b4f9d8de0a7b2ff8d8798fa7808953d
Adam <scatty101@gmail.com>
2021-01-31 21:50:32 +0100

    Created toolchain file. Added toolchain description in readme. CMake is working from cmds

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <cstdint>
+
+int main(void)
+{
+    return 0;
+}
---
+++ toolchain.cmake
@@ -0,0 +1,34 @@
+# We are cross compileing
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+# We use bare metal
+set(TOOLCHAIN_PREFIX arm-none-eabi-)
+
+# Cmd for win cmd, can be added unix
+set(SEARCH_CMD where)
+
+# Find toolchain
+execute_process(
+    COMMAND ${SEARCH_CMD} ${TOOLCHAIN_PREFIX}gcc
+    OUTPUT_VARIABLE TOOLCHAIN_PATH 
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# Get directory
+get_filename_component(ARM_TOOLCHAIN_PATH ${TOOLCHAIN_PATH} DIRECTORY)
+
+# Try to compile static library
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+
+# Set compilers
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
+
+
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PATH})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
